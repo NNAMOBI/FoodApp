e55970440c9e0077a0c961e05de094115b2d18cb
NNAMOBI <nnamosuagwu84@gmail.com>
2022-02-16 17:01:24 +0000

    Add project files.

---
+++ odeFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using OdeFood.Data.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odeFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();  // get all the list of restaurant
+    }
+}
---
+++ odeFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using odeFood.Data.Model;
+using OdeFood.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace odeFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        /// <summary>
+        /// creating a constructor to initialise a list of restaurants
+        /// </summary>
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Pepe's Pizza", CuisineType = CuisineType.Indian},
+                new Restaurant { Id = 2, Name = "Nacos", CuisineType = CuisineType.Italian},
+                new Restaurant { Id = 3, Name = "kajie res", CuisineType = CuisineType.French}
+            };
+           }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);                  // This is possible because the IRestauranData has a contract for implementation of IEnumberable
+        }                                                             // order by name
+    }
+}
